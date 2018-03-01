690a6850adff76da962710cd2f8f5811596bca70
wesbosse <wes.bosse@gmail.com>
2018-02-28 18:07:18 -0800

    grid searching updated for big rig

--- digit_recognition.py
+++ digit_recognition.py
@@ -1,12 +1,19 @@
+import numpy as np
 import pandas as pd
 
+from sklearn.datasets import load_breast_cancer, fetch_lfw_people
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
 from keras.models import Sequential
 from keras.wrappers.scikit_learn import KerasClassifier
-from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
+from keras.utils import to_categorical
 
+import matplotlib.pyplot as plt
+% matplotlib
+inline
 
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
@@ -18,47 +25,56 @@ X = X.values / 255.
 X = X.reshape(-1, 28, 28, 1)
 y = train['label']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=5)
 
 
-def model_func(n1=30, s1=4, p1=2, n2=40, s2=3, p2=2, n3=30, n4=10):
+def model_func(n1=30, d1=2.5, s1=5, p1=2, n2=40, d2=.25, s2=3, p2=2, n3=200, d3=.5, n4=10):
     model = Sequential()
     model.add(Conv2D(n1, (s1, s1), input_shape=(28, 28, 1), activation='relu'))
     model.add(MaxPool2D((p1, p1)))
+    model.add(Dropout(0.25))
     model.add(Conv2D(n2, (s2, s2), activation='relu'))
     model.add(MaxPool2D((p2, p2)))
+    model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(n3, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(n4, activation='softmax'))
 
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'], )
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
 
 
-model = KerasClassifier(build_fn=model_func, epochs=15, verbose=1)
+model = KerasClassifier(build_fn=model_func, epochs=12, verbose=1)
 
 pipe = Pipeline([
     ('model', model)
 ])
 
+
 params = {
-        'model__epochs': [12, 15, 18],
-        'model__n1': [30, 40],
-        'model__s1': [4,5,6],
-        'model__p1': [1,2,3],
-        'model__n2': [40,60],
-        'model__s2': [3,4,5],
-        'model__p2': [1,2,3],
-        'model__n3': [15,30]
+    'model__epochs': [20],
+    'model__n1': [30, 50, 80],
+    'model__d1': [.2, .35, .5],
+    'model__s1': [3,4,5],
+    'model__p1': [1,2,3],
+    'model__n2': [30,60],
+    'model__d2': [.2, .35, .5],
+    'model__s2': [2,3,4],
+    'model__p2': [1,2,3],
+    'model__n3': [10,50,200],
+    'model__d3': [.4, .5, .6]
 }
 
-gs = GridSearchCV(pipe, param_grid=params)
+gs = GridSearchCV(pipe, param_grid=params, cv=5)
 gs.fit(X_train, y_train)
+
 
 print('Train Score: ', gs.best_score_)
 print('Best Param Dict: ', gs.best_params_)
-print('Test Score: ', gs.score(X_test, y_test))
+print('Test Score: ', gs.score(X_test,y_test))
+
 
 X_pred = test
 X_pred = X_pred.values / 255.
