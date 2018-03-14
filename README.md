189b9c4bc786d4e2bd76929139ef50f5b06a4f0f
wesbosse <wes.bosse@gmail.com>
2018-03-14 14:51:27 -0700

    changing file structure

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Digit-Recognition-Kaggle-Competition
 Convolutional Neural Networks in Keras to detect numeric digits from images. Grid searched and cross validated using AWS EC2 instance
+
+##Competition Description
+MNIST ("Modified National Institute of Standards and Technology") is the de facto “hello world” dataset of computer vision. Since its release in 1999, this classic dataset of handwritten images has served as the basis for benchmarking classification algorithms. As new machine learning techniques emerge, MNIST remains a reliable resource for researchers and learners alike.
+
+In this competition, your goal is to correctly identify digits from a dataset of tens of thousands of handwritten images. We’ve curated a set of tutorial-style kernels which cover everything from regression to neural networks. We encourage you to experiment with different algorithms to learn first-hand what works well and how techniques compare.
+
+##Practice Skills
+-Computer vision fundamentals including simple neural networks
+-Classification methods such as SVM and K-nearest neighbors
--- digit_recognition.py
+++ digit_recognition.py
@@ -6,8 +6,8 @@ from keras.models import Sequential
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
 
-train = pd.read_csv('train.csv')
-test = pd.read_csv('test.csv')
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
 
 X = train.drop(columns=['label'])
 X = X.values / 255.
@@ -17,26 +17,28 @@ y = train['label']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
 
-def model_func(n1=30, d1=.25, s1=5, p1=2, n2=40, d2=.25, s2=3, p2=2, n3=200, d3=.5, n4=10):
+def model_func():
 
     model = Sequential()
-    model.add(Conv2D(n1, (s1, s1), input_shape=(28, 28, 1), activation='relu'))
-    model.add(MaxPool2D((p1, p1)))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(n2, (s2, s2), activation='relu'))
-    model.add(MaxPool2D((p2, p2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(n3, activation='relu'))
+    model.add(Conv2D(32, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+    model.add(Conv2D(32, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+    model.add(MaxPool2D((2, 2)))
     model.add(Dropout(0.5))
-    model.add(Dense(n4, activation='softmax'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPool2D((2, 2)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(250, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
 
 
-model = KerasClassifier(build_fn=model_func, epochs=12, verbose=1)
+model = KerasClassifier(build_fn=model_func, epochs=16, verbose=1)
 
 pipe = Pipeline([
     ('model', model)
@@ -44,20 +46,20 @@ pipe = Pipeline([
 
 
 params = {
-    'model__epochs': [20],
-    'model__n1': [30, 80],
-    'model__d1': [.25, .5],
-    'model__s1': [4],
-    'model__p1': [2],
-    'model__n2': [30, 60],
-    'model__d2': [.25, .5],
-    'model__s2': [3],
-    'model__p2': [2],
-    'model__n3': [50,200],
-    'model__d3': [.25, .5]
+    # 'model__epochs': [20],
+    # 'model__n1': [30, 80],
+    # 'model__d1': [.25, .5],
+    # 'model__s1': [4],
+    # 'model__p1': [2],
+    # 'model__n2': [30, 60],
+    # 'model__d2': [.25, .5],
+    # 'model__s2': [3],
+    # 'model__p2': [2],
+    # 'model__n3': [50,200],
+    # 'model__d3': [.25, .5]
 }
 
-gs = GridSearchCV(pipe, param_grid=params, cv=3)
+gs = GridSearchCV(pipe, param_grid=params, cv=5)
 gs.fit(X_train, y_train)
 
 
